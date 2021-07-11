2093b312668e0f7f5cd57da211c94a3ec5298cf7
YouwenLiang <layouwen@gmail.com>
2021-07-11 10:06:25 +0800

    实现 router-link 组件

--- src/avue-router.js
+++ src/avue-router.js
@@ -13,6 +13,30 @@ VueRouter.install = function(_Vue) {
       if (this.$options.router) Vue.prototype.$router = this.$options.router
     },
   })
+  Vue.component('router-link', {
+    props: {
+      to: {
+        type: String,
+        require: true,
+      },
+    },
+    render(h) {
+      return h(
+        'a',
+        {
+          attrs: {
+            href: '#' + this.to,
+          },
+        },
+        this.$slots.default
+      )
+    },
+  })
+  Vue.component('router-view', {
+    render(h) {
+      return h('div', '我是router-view')
+    },
+  })
 }
 
 export default VueRouter
